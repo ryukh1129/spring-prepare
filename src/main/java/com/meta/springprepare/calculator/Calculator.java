c388e71c29df9fe52c8051e8622147c17eb65ab7
ryukh1129 <ryukh1129@gmail.com>
2026-01-12 10:47:49 +0900

    docs: application.properties 서버 설정 변경 테스트

--- src/main/java/com/meta/springprepare/calculator/Calculator.java
+++ src/main/java/com/meta/springprepare/calculator/Calculator.java
@@ -1,6 +1,23 @@
 package com.meta.springprepare.calculator;
 
 public class Calculator {
+    // 한줄 주석
+
+    /*
+    ㅁ
+    ㅁ
+    여러 문장 주석
+     */
+
+    /**
+     * 계산기 사칙 연산 함수
+     * @param num1 : 입력값 1
+     * @param op : 연산자
+     * @param num2 : 입력값 2
+     * @return : result
+     */
+
+    // 사칙 연산 기능
     public Double operate(double num1, String op, double num2) {
         switch (op) {
             case "*":
