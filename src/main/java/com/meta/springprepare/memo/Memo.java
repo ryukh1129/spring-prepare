2701310339c415a0d34db79c6a9a142a3f6eb04f
ryukh1129 <ryukh1129@gmail.com>
2026-01-12 11:47:40 +0900

    chore: Lombok을 기본생성자, 모든 인수를 받는 생성자, 필수인수를 받는 생성자

--- src/main/java/com/meta/springprepare/memo/Memo.java
+++ src/main/java/com/meta/springprepare/memo/Memo.java
@@ -1,20 +1,21 @@
 package com.meta.springprepare.memo;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Memo {
-    private String username;
+    private final String username;
     private String title;
     private String contents;
     private Date createAt;
     private Date updatedAt;
 
     public void writeMemo() {
-
     }
 }
